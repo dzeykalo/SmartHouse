2547589177d4b54f8e07d82df3d6d25a35f044de
Toxa <mr.alligator@yandex.ru>
2025-10-27 01:30:12 +0300

    Adds fake thermometer application

--- src/fake_thermometer/mod.rs
+++ src/fake_thermometer/mod.rs
@@ -1,14 +1,51 @@
+use std::fs;
 use std::net::UdpSocket;
-use std::io::{self, Write};
+use std::process::exit;
 use std::thread::sleep;
 use std::time::Duration;
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    host: String,
+    port: u16,
+    interval_ms: u64,
+}
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <config-file.json>", args[0]);
+        exit(1);
+    }
+
+    let config_file_path = args[1].to_string();
+    let data = match fs::read_to_string(args[1].to_string()) {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("Не удалось прочитать файл '{}': {}", config_file_path, e);
+            exit(1);
+        }
+    };
+    
+    let cfg: Config = match serde_json::from_str(&data) {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            eprintln!("Ошибка парсинга JSON в файле '{}': {}", config_file_path, e);
+            exit(1);
+        }
+    };
+
+    let server_address = format!("{}:{}", cfg.host, cfg.port);
     let socket = UdpSocket::bind("0.0.0.0:0").expect("Could not bind socket");
-    let server_address = "127.0.0.1:8080";
     loop {
-        let message = "23.2";
-        socket.send_to(message.as_bytes(), server_address).expect("Error send to thermometer server");
-        sleep(Duration::from_secs(1));
+        let random_temperature: f32 = rand::rng().random_range(18.0..=26.0);
+        let message = format!("{:.2}",random_temperature);
+        socket.send_to(message.as_bytes(), &server_address)
+            .expect("Error send to thermometer server");
+        sleep(Duration::from_millis(cfg.interval_ms));
     }
 }
